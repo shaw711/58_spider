44a9f3bfb97902b7b1d11c2724bca754f08be01a
shaw <1051400867@qq.com>
2018-12-07 15:21:59 +0800

    check updated for second hand car.

--- app/html_parser.py
+++ app/html_parser.py
@@ -10,9 +10,11 @@ class HtmlParser(object):
 	def _get_new_urls(self, soup):
 		new_urls = set()
 		
-		links = soup.find_all('a', href=re.compile(r'http://gz.58.com/ershouche/.*entinfo='))
+		links = soup.find_all('a', href=re.compile(r'https://gz.58.com/ershouche/.*shtml\?sourceKey'))
+		# print(links)
 		for link in links:
 			new_url = link['href']
+#			print(new_url)
 			new_urls.add(new_url)
 		return new_urls
 		
@@ -20,21 +22,24 @@ class HtmlParser(object):
 		res_data = {}
 		
 		res_data['url'] = page_url
+#		print('@@@@@')
+#		print(page_url)
+#		print('@@@@')
 		
-		title_node = soup.find('div', id="content_sumary_right").find('h1')
+		title_node = soup.find('div', {"class":'content_title'}).find('p',{"class":'title_p'})
 		res_data['title'] = title_node.get_text()
 		
-		price_node = soup.find('span', class_="font_jiage")
+		price_node = soup.find('span', class_="jiage")
 		
-		price = re.match(r'^¥\s+(\d+\.\d+)', price_node.get_text())
-		res_data['price'] = float(price.group(1))
+		price = re.match(r'\d+\.*\d*', price_node.get_text())
+		res_data['price'] = float(price.group(0))
 		
 		return res_data
 	
 	def parse_urls(self, html_cont):
 		if html_cont is None:
 			return
-			
+		# print(html_cont)
 		soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
 		new_urls = self._get_new_urls(soup)
 		return new_urls 
--- app/spider_main.py
+++ app/spider_main.py
@@ -12,14 +12,17 @@ class SpiderMain(object):
 		
 	def craw(self, root_url):
 		for i in range(1, 71):
+#		for i in range(1, 2):
 			full_url = root_url + str(i)
 			print('craw :', i, full_url)
 			html_page_cont = self.downloader.download(full_url)
 			new_urls = self.parser.parse_urls(html_page_cont)
 			for url in new_urls:
 				print('craw', i, url)
+#				url = r"https://gz.58.com/ershouche/36361926194843x.shtml?sourceKey=NDHas1YqwzbLCCPRFSICniL5t44tYwnc04dVZHpLEaN3O2aOTiD54ddUWWfqTuWX6AoIlDGkVYbTZR7B2DtXv3BRh94p9ccbYIBAEVIjaHI&page_id=501&slot_id=31&queryUniqueId=1070932275111186432&typos=advertInfo_l117&infotype=advert_2_2&&"
 				try:
 					html_cont = self.downloader.download(url)
+					# print(html_cont)
 					data = self.parser.parse_data(url, html_cont)
 					print(data)
 					self.outputer.collect_data(data)
